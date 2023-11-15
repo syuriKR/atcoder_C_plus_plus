ce739b75901cd4f15853ee0ece27cedf485ca2ee
chocoberry <s1911460@klis.tsukuba.ac.jp>
2023-11-15 17:56:38 +0900

    adt_easy_20231114_2

---
+++ atcoder/other/adt_easy_20231114_2/c/adt_easy_20231114_2_c.cpp
@@ -0,0 +1,36 @@
+// clang-format off
+#include<bits/stdc++.h>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+
+using namespace std;
+
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+// clang-format on
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> vec(n);
+    rep(i, 0, n) {
+        cin >> vec[n];
+    }
+    rep(i, 0, m) {
+        int num;
+        cin >> num;
+        auto itr = find(vec.begin(), vec.end(), num);
+        if(itr==vec.end() && num!=vec[*itr]){
+            cout << "No" << endl;
+            return 0;
+        }
+        vec[*itr]=0;
+
+    }
+    cout << "Yes" << endl;
+    return 0;
+}
