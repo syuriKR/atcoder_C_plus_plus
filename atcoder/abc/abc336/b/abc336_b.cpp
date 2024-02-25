225c4b27e612bbf5e82f9c35f904107393ec8e7e
chocoberry <s1911460@klis.tsukuba.ac.jp>
2024-02-25 19:06:21 +0900

    wip abc336

---
+++ atcoder/abc/abc336/b/abc336_b.cpp
@@ -0,0 +1,28 @@
+// clang-format off
+#include<bits/stdc++.h>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+
+using namespace std;
+
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+// clang-format on
+
+int main() {
+    int n;
+    cin >> n;
+    int ans = 0;
+    while (true) {
+        if (n % 2 == 0) {
+            ans++;
+            n /= 2;
+        } else
+            break;
+    }
+    cout << ans << endl;
+    return 0;
+}
