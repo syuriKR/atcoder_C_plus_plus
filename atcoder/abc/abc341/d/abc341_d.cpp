0ed8d6f0c9b404f00f897fbd7059c3644bec86e1
chocoberry <s1911460@klis.tsukuba.ac.jp>
2024-03-10 22:44:01 +0900

    add abc341d

--- atcoder/abc/abc341/d/abc341_d.cpp
+++ atcoder/abc/abc341/d/abc341_d.cpp
@@ -12,6 +12,32 @@ template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } 
 template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
 // clang-format on
 
+ll lcm(ll a, ll b) {
+    return a * b / gcd(a, b);
+}
+
 int main() {
+    ll n, m, k;
+    cin >> n >> m >> k;
+    ll lcm_num = lcm(n, m);
+    // loupの個数で公倍にぶつかる、n=2, m=3とかなら(2-1)+(3-1)=3でループ (2,3,4) (8,9,10)
+    ll loup = (lcm_num / n - 1) + (lcm_num / m - 1);
+    ll start = k / loup * lcm_num;
+    ll nokori = k - (k / loup) * loup;
+    if (nokori == 0) {
+        start -= min(n, m);
+    } else {
+        ll n_now = n;
+        ll m_now = m;
+        for (int i = 0; i < nokori - 1; i++) {
+            if (n_now < m_now) {
+                n_now += n;
+            } else {
+                m_now += m;
+            }
+        }
+        start += min(n_now, m_now);
+    }
+    cout << start << endl;
     return 0;
 }
