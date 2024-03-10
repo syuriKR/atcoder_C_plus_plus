f225c075237a14429c59fc260ce9b1f0abfc5782
chocoberry <s1911460@klis.tsukuba.ac.jp>
2024-03-10 23:44:01 +0900

    add abd342d

--- atcoder/abc/abc342/d/abc342_d.cpp
+++ atcoder/abc/abc342/d/abc342_d.cpp
@@ -13,5 +13,40 @@ template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } 
 // clang-format on
 
 int main() {
+    ll n;
+    cin >> n;
+    // ここのmapを<int,int>にしてるとWAになる。
+    // 下のmap.second*(map.second-1)のところでintの範囲超えそう
+    // map.secondの最大値がnのため、n**2で4*10**10になる。
+    map<int, ll> map;
+    for (ll i = 0; i < n; i++) {
+        ll a;
+        cin >> a;
+        if (a != 0) {
+            for (int i = sqrt(200000) + 1; i > 0; i--) {
+                if (a % (i * i) == 0) {
+                    a /= (i * i);
+                }
+            }
+        }
+
+        if (map.count(a)) {
+            map[a]++;
+        } else {
+            map[a] = 1;
+        }
+    }
+
+    ll ans = 0;
+    fore(a, map) {
+        if (a.first == 0) {
+            for (ll i = 1; i <= a.second; i++) {
+                ans += n - i;
+            }
+        } else {
+            ans += a.second * (a.second - 1) / 2;
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
